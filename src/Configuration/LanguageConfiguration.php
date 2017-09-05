88aa1324dae804e921c72b136b612f389357cdbe
Michal Katuščák <michal@katuscak.cz>
2017-09-05 20:55:46 +0200

    Add PHPDoc comments

--- src/Configuration/LanguageConfiguration.php
+++ src/Configuration/LanguageConfiguration.php
@@ -4,6 +4,13 @@ namespace Gephart\Language\Configuration;
 
 use Gephart\Configuration\Configuration;
 
+/**
+ * Language configuration
+ *
+ * @package Gephart\Language\Configuration
+ * @author Michal Katuščák <michal@katuscak.cz>
+ * @since 0.3
+ */
 class LanguageConfiguration
 {
     /**
@@ -16,6 +23,10 @@ class LanguageConfiguration
      */
     private $directory;
 
+    /**
+     * @param Configuration $configuration
+     * @throws \Exception
+     */
     public function __construct(Configuration $configuration)
     {
         $language = $configuration->get("language");
@@ -28,11 +39,18 @@ class LanguageConfiguration
         $this->directory = $configuration->getDirectory();
     }
 
+    /**
+     * @param string $key
+     * @return bool|mixed
+     */
     public function get(string $key)
     {
         return isset($this->language[$key]) ? $this->language[$key] : false;
     }
 
+    /**
+     * @return string
+     */
     public function getDirectory(): string
     {
         return $this->directory;
--- src/Language.php
+++ src/Language.php
@@ -4,22 +4,40 @@ namespace Gephart\Language;
 
 use Gephart\Language\Configuration\LanguageConfiguration;
 
+/**
+ * Language
+ *
+ * @package Gephart\Language
+ * @author Michal Katuščák <michal@katuscak.cz>
+ * @since 0.3
+ */
 final class Language
 {
 
-    /** @var string */
+    /**
+     * @var string
+     */
     private $language;
 
+    /**
+     * @param LanguageConfiguration $language_configuration
+     */
     public function __construct(LanguageConfiguration $language_configuration)
     {
         $this->language = $language_configuration->get("fallback");
     }
 
+    /**
+     * @return string
+     */
     public function get()
     {
         return $this->language;
     }
 
+    /**
+     * @param string $language
+     */
     public function set(string $language)
     {
         $this->language = $language;
